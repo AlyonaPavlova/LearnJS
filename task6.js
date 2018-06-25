9cbdd4ce36721ae0e2ac7569d4d7f69e8a489212
Alena Pavlova <alyonabill3@mail.ru>
2018-06-25 22:21:03 +0300

    task6 was added

--- task6.js
+++ task6.js
@@ -6,58 +6,13 @@ var tailLengths = [10, 20, 30];
 
 
 
-function catFactory(paramObject) {
+function catFactory(defaults) {
 
-
-    // var defaults = {
-    //     name: names,
-    //     age: ages,
-    //     gender: genders,
-    //     legsCount: legsCounts,
-    //     tailLength: tailLengths
-    // };
-
-    // var finalParams = defaults;
-
-    // for (var key in paramObject) {
-    //
-    //     if (paramObject.hasOwnProperty(key)) {
-    //
-    //         if (paramObject[key] !== undefined) {
-    //
-    //             finalParams[key] = paramObject[key];
-    //         }
-    //     }
-    // }
-
-    // console.log(finalParams.name, finalParams.age, finalParams.gender, finalParams.legsCount, finalParams.tailLength);
-
-
-    // if (name === undefined) {
-    //     name = names;
-    // }
-    //
-    // if (age === undefined) {
-    //     age = ages;
-    // }
-    //
-    // if (gender === undefined) {
-    //     gender = genders;
-    // }
-    //
-    // if (legsCount === undefined) {
-    //     legsCount = legsCounts;
-    // }
-    //
-    // if (tailLength === undefined) {
-    //     tailLength = tailLengths;
-    // }
-
-    var nameRand = paramObject.name || names[Math.floor(Math.random() * names.length)];
-    var ageRand = paramObject.age || ages[Math.floor(Math.random() * ages.length)];
-    var genderRand = paramObject.gender || genders[Math.floor(Math.random() * genders.length)];
-    var legsCountRand = paramObject.legsCount || legsCounts[Math.floor(Math.random() * legsCounts.length)];
-    var tailLengthRand = paramObject.tailLength || tailLengths[Math.floor(Math.random() * tailLengths.length)];
+    var nameRand = defaults.name || names[Math.floor(Math.random() * names.length)];
+    var ageRand = defaults.age || ages[Math.floor(Math.random() * ages.length)];
+    var genderRand = defaults.gender || genders[Math.floor(Math.random() * genders.length)];
+    var legsCountRand = defaults.legsCount || legsCounts[Math.floor(Math.random() * legsCounts.length)];
+    var tailLengthRand = defaults.tailLength || tailLengths[Math.floor(Math.random() * tailLengths.length)];
 
     var obj = {
         name: nameRand,
@@ -77,10 +32,10 @@ function objViewer(obj) {
     return output;
 }
 
-function catsGroupGenerate(n, paramObject) {
+function catsGroupGenerate(n, defaults) {
     var catsArray = [];
     while(catsArray.length < n) {
-        var myCat = catFactory(paramObject);
+        var myCat = catFactory(defaults);
         catsArray.push (myCat);
     }
     return catsArray;
