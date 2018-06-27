5daf0d11c799bc9e7eee9681ee4ebec0451a7d96
Unknown <alyonabill3@mail.ru>
2018-06-27 14:48:18 +0300

    test for task2 and task3 was added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+const { assert } = require('chai');
 
 describe('Array', function() {
 
@@ -11,59 +11,62 @@ describe('Array', function() {
 
     it('should return cat with random values', function(){
 
+        var names = ["Max","Smokey", "Oliver", "Baddie", "Charlie", "Oscar", "Lucie", "Mollie", "Bella", "Sophie", "Lily", "Maggie"];
+
+        var obj = {};
+
+        function catFactory(name) {
+
+
+                obj.name = name[Math.floor(Math.random() * name.length)];
+
+            return obj;
+        }
+
+        assert.include(catFactory(names),  /^name/);
+
+    });
+
+    it('should return group cats (n) with random values', function () {
+
+        var names = ["Max","Smokey", "Oliver", "Baddie", "Charlie", "Oscar", "Lucie", "Mollie", "Bella", "Sophie", "Lily", "Maggie"];
+        var ages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        var genders = ["female", "male"];
+        var legsCounts = [1, 2, 3, 4];
+        var tailLengths = [10, 20, 30];
+
         function catFactory(name, age, gender, legsCount, tailLength) {
 
+            var nameRand = name[Math.floor(Math.random() * name.length)];
+            var ageRand = age[Math.floor(Math.random() * age.length)];
+            var genderRand = gender[Math.floor(Math.random() * gender.length)];
+            var legsCountRand = legsCount[Math.floor(Math.random() * legsCount.length)];
+            var tailLengthRand = tailLength[Math.floor(Math.random() * tailLength.length)];
+
             var obj = {
-                name: name,
-                age: age,
-                gender: gender,
-                legsCount: legsCount,
-                tailLength: tailLength
+                name: nameRand,
+                age: ageRand,
+                gender: genderRand,
+                legsCount: legsCountRand,
+                tailLength: tailLengthRand
             };
             return obj;
         }
 
-        var name = "Max";
-        var age = 2;
-        var gender = "male";
-        var legsCount = 2;
-        var tailLength = 2;
+        var catsArray = [];
+        function catsGroupGenerate(n) {
 
-        assert.equal(
-            {
-                name: "Max",
-                age: 2,
-                gender: "male",
-                legsCount: 2,
-                tailLength: 2
+            while(catsArray.length < n) {
+                var myCat = catFactory(names, ages, genders, legsCounts, tailLengths);
+                catsArray.push (myCat);
+            }
+            return catsArray;
+        }
 
-            }, catFactory(name, age, gender, legsCount, tailLength));
+        catsGroupGenerate(5);
+
+        assert(catsArray.length === 5);
+
     });
 
 });
-
-
-
-
-
-
-
-// var names = ["Max","Smokey", "Oliver", "Baddie", "Charlie", "Oscar", "Lucie", "Mollie", "Bella", "Sophie", "Lily", "Maggie"];
-// var ages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// var genders = ["female", "male"];
-// var legsCounts = [1, 2, 3, 4];
-// var tailLengths = [10, 20, 30];
-//
-// function catFactory() {
-//
-//     var obj = {
-//         name: names[Math.floor(Math.random() * names.length)],
-//         age: ages[Math.floor(Math.random() * ages.length)],
-//         gender: genders[Math.floor(Math.random() * genders.length)],
-//         legsCount: legsCounts[Math.floor(Math.random() * legsCounts.length)],
-//         tailLength: tailLengths[Math.floor(Math.random() * tailLengths.length)]
-//     };
-//     return obj;
-// }
-//
-// assert(catFactory());
