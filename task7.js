9c6cc0d0987b64044992e210ca0f1aeda115beb6
Unknown <alyonabill3@mail.ru>
2018-06-29 10:37:58 +0300

    File with function was added

---
+++ task7.js
@@ -0,0 +1,31 @@
+const { pick } = require("./functions");
+
+function catFactoryDef(defaults) {
+
+    var obj = {
+        name: defaults ? defaults.name: pick(["Max","Smokey", "Oliver", "Baddie", "Charlie", "Oscar", "Lucie", "Mollie", "Bella", "Sophie", "Lily", "Maggie"]),
+        age: defaults ? defaults.age: pick([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        gender: defaults ? defaults.gender: pick(["female", "male"]),
+        legsCount: defaults ? defaults.legsCount: pick([1, 2, 3, 4]),
+        tailLength: defaults ? defaults.tailLength: pick([10, 20, 30])
+    };
+    return obj;
+}
+
+function catsGroupGenerate(n, defaults) {
+
+    var catsArray = [];
+
+    while(catsArray.length < n) {
+        var myCat = catFactoryDef(defaults);
+        catsArray.push(myCat);
+    }
+    return catsArray;
+}
+
+console.log(catsGroupGenerate(10));
+
+module.exports = {
+
+    catFactoryDef
+};
